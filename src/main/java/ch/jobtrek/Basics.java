5bfb8dd7c290840b9fc399575396fb7cedba0184
Rignchen <rinchendemort@gmail.com>
2023-10-06 14:01:13 +0200

    Basics.someConditionals

---
+++ src/main/java/ch/jobtrek/Basics.java
@@ -0,0 +1,38 @@
+package ch.jobtrek;
+
+public class Basics {
+
+    /**
+     * This function gets two integers and must compute them as follows :
+     * IF all numbers are even or number 1 superior to 10, return true
+     * in all other cases, return false
+     */
+    public static boolean someConditionals(int number1, int number2) {
+        return number1>10||(number1%2==0&&number2%2==0);
+    }
+
+    /**
+     * @param text A string containing words separated by spaces
+     * @return A string containing only words of maximum 4 characters and starting
+     * with letter T. All words should be uppercase and separated by spaces
+     */
+    public static String strings(String text) {
+        String[] words = text.split(" ");
+        text = "";
+        for (String word: words) {
+            if (word.length() <= 4 && word.toLowerCase().startsWith("t"))
+                text += word.toUpperCase();
+        }
+        return text.substring(0, text.length()-1);
+    }
+
+    /**
+     * @param needle The word you need to replace
+     * @param haystack A string in which you need to replace the needle by the new word
+     * @param newWord The replacement word
+     * @return A new string where the needle is replaced by newWord
+     */
+    public static String findAndReplace(String needle, String haystack, String newWord) {
+        return haystack.replaceAll(needle,newWord);
+    }
+}
---
+++ src/main/java/ch/jobtrek/Collections.java
@@ -0,0 +1,50 @@
+package ch.jobtrek;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Collections {
+
+    /**
+     * @param array An array that contains strings of different lengths
+     * @param size  The string size used to chunk the array
+     * @return An array containing two arrays, one with the stings below and equal
+     * of the size, and another with the stings longer than the size
+     */
+    public static List<List<String>> chunkArrayByStringSize(List<String> array, int size) {
+        List<List<String>> output = new ArrayList<List<String>>();
+        output.add(new ArrayList<String>());
+        output.add(new ArrayList<String>());
+        for (String word:array) {
+            //if (word.length() > size) output[1].add(word);
+        }
+        return output;
+    }
+
+    /**
+     * @param numbers A list of integers
+     * @return A list containing the integers of the original list, without
+     * duplicates, sorted by frequency of occurrence
+     */
+    public static List<Integer> frequencyOfApparition(List<Integer> numbers) {
+        return List.of();
+    }
+
+    /**
+     * @param numbers A list, containing list of numbers
+     * @return The sum of all numbers, but all odd numbers should be multiplied before sum
+     * Should return zero if there is no numbers
+     */
+    public static Integer sumArrays(List<List<Integer>> numbers) {
+        return 1;
+    }
+
+    /**
+     * @param students A hashmap containing students name as key, and student grade as value
+     * @return The student with the best grade in the map
+     */
+    public static String bestStudent(Map<String, Integer> students) {
+        return new String();
+    }
+}
